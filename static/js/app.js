203f062fcede0ddc5ff132e0dead2ea3132297cc
Justin K <justinhkiang@gmail.com>
2022-04-04 14:39:59 -0700

    use d3 to listen for when users click on the filter button, which will trigger our new function which filters the table data based on user entered data parameters

--- static/js/app.js
+++ static/js/app.js
@@ -8,12 +8,10 @@ var tbody = d3.select("tbody");
 function buildTable(data) {
     // At the start of our function, clear out any existing data (like a "reset")
     tbody.html("");
-
     // loop through each object in the data & append a row and cells for each value in the row
     data.forEach((dataRow) => {
         // append a row to the table body
         let row = tbody.append("tr");
-
         // loop through each field in the row and add each value as a table cell (table data or 'td')
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
@@ -22,3 +20,23 @@ function buildTable(data) {
         });
     });
 }
+
+// function to rebuild a filtered table if a date is entered as a filter criteria
+function handleClick() {
+    // grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    // check to see if a date was entered and filter the data using that date
+    if (date) {
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    // rebuild the table using the filtered version of the data
+    // @NOTE: if no date was entered, then filteredData will just be the original tableData
+    buildTable(filteredData);
+}
+
+// use D3 to 'listen' for an event (in this case, the 'click' of our filtering button)
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// At the bare minimum - show the base table before users have an oppty to input any filter criteria
+buildTable(tableData);
